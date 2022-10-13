c8485a8c6761af212ccdd6b3418ad4309b19ebb8
Oleg_bul <kokseg63@gmail.com>
2022-10-14 00:40:57 +0400

    Task 58 complete

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -1,9 +1,32 @@
-﻿int matrX = ReadData("Ввведите размер M ");
-int matrY = ReadData("Введите размер N ");
-int[,] matrix = new int[matrX, matrY];
+﻿// Задача 58: Задайте две матрицы.
+// Напишите программу, которая будет 
+// находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int size = ReadData("Ввведите размер массивов ");
+int[,] matrixA = new int[size,size];
+int[,] matrixB = new int[size,size];
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+            matrix[i, j] = new Random().Next(1, 10);
+    }
+}
 void PrintArray(int[,] matr)
 {
-    for (int i = 0; i < matr.GetLength(0); i++) 
+    for (int i = 0; i < matr.GetLength(0); i++)
     {
         for (int j = 0; j < matr.GetLength(1); j++)
         {
@@ -13,41 +36,27 @@ void PrintArray(int[,] matr)
     }
 }
 
-
-int ReadData(string msg)
+int [,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
 {
-    Console.Write(msg);
-    int number = int.Parse(Console.ReadLine());
-    return number;
+    int[,] multiMatrix = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+            for (int i = 0; i < matrixA.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixB.GetLength(1); j++)
+                {
+                    for (int k = 0; k < matrixB.GetLength(0); k++)
+                    {
+                        multiMatrix[i,j] += matrixA[i,k] * matrixB[k,j];
+                    }
+                }
+            }
+            return multiMatrix;
 }
+FillArray(matrixA);
+FillArray(matrixB);
+System.Console.WriteLine("Матрица А");
+PrintArray(matrixA);
+System.Console.WriteLine("Матрица B");
+PrintArray(matrixB);
+System.Console.WriteLine("Произведение матриц А и В");
+PrintArray(MatrixMultiplication(matrixA,matrixB));
 
-void FillArray(int[,] matrix)
-{
-    for (int i = 0; i < matrX; i++)
-    {
-      for (int j = 0; j < matrY; j++)
-        matrix [i,j] = new Random().Next(0,100);
-    }
-}
-
-void ArithmeticMeanOfTheColumn(int[,]matrix)
-{
-    
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        double sum = 0;
-        double aritMean = 0;
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            sum = sum + matrix[j,i];
-            
-        }
-        aritMean = sum /(matrix.GetLength(1));
-        Console.WriteLine(" Среднее арифметическое столбцa "+ (i+1)+":");
-        Console.WriteLine(" "+ Math.Round(aritMean,1));
-        
-    }
-}
-FillArray(matrix);
-PrintArray(matrix);
-ArithmeticMeanOfTheColumn(matrix);
